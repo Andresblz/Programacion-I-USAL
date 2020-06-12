eff7cc55c347ddfa890a14d2f8bd99d2262b19f2
Andresblz <57406530+Andresblz@users.noreply.github.com>
2020-06-12 15:08:15 +0200

    Add files via upload

---
+++ Tema 3/05_Propuesto/main.c
@@ -0,0 +1,28 @@
+/*
+ *  All Rights Reserved.
+ *  © 2020 Andrés Blázquez Colino
+ *  andresblz@usal.es
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void){
+    float num;
+
+    do{
+        printf("Introduce un numero real mayor que cero: ");
+        scanf("%f", &num);
+    }while(num <= 0);
+
+    printf("\nNúmero introducido..........: %.2f", num);
+    printf("\nCuadrado....................: %.4f", pow(num,2));
+    printf("\nCubo........................: %.4f", pow(num,3));
+    printf("\nLogaritmo neperiano.........: %.4f", log(num));
+    printf("\nLogaritmo decimal...........: %.4f", log10(num));
+    printf("\nRaiz cuadrada...............: %.4f", sqrt(num));
+
+    printf("\n\n");
+    system("pause");
+    return 0;
+}
