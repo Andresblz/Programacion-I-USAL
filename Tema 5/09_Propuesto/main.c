13a100e0ca25c93aa9bcf7b7846fcd8cd3384ba8
Andresblz <57406530+Andresblz@users.noreply.github.com>
2020-06-12 23:01:47 +0200

    Add files via upload

---
+++ Tema 5/09_Propuesto/main.c
@@ -0,0 +1,32 @@
+/*
+ *  All Rights Reserved.
+ *  © 2020 Andrés Blázquez Colino
+ *  andresblz@usal.es
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    const char blanco = ' ';
+    char car;
+    int alt;
+
+    printf("Introduce caracter: ");
+    scanf("%c", &car);
+    printf("Introduce altura: ");
+    scanf("%d", &alt);
+
+    for(int i = 0; i <= alt; i++){
+        for(int j = 0; j < alt-i; j++){
+            printf("%c", blanco);
+        }
+        for(int j = 0; j < i; j++){
+            printf("%c", car);
+        }
+        printf("\n");
+    }
+
+    printf("\n\n");
+    system("pause");
+    return 0;
+}
